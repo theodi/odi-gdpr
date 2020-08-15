82cdb6a458c9e2b47627c025e505b705e92c5837
David Tarrant <davetaz@theodi.org>
2020-08-15 12:25:49 +0100

    Update to v5

--- adapt/js/scriptLoader.js
+++ adapt/js/scriptLoader.js
@@ -1,119 +1,149 @@
 (function() {
 
-    //Test for ie8
-    var IE = (function() {
-        if (document.documentMode) {
-            return document.documentMode;
+  // Change location of Adapt CSS if incorrect
+  (function () {
+    var oldHRef = 'adapt/css/adapt.css';
+    var newHRef = 'adapt.css';
+    function fixCSSLocation() {
+      var oldLinkElement = findOldLink();
+      if (!oldLinkElement) return;
+      replaceOldLink(oldLinkElement);
+    }
+    function findOldLink() {
+      var nodeList = document.querySelectorAll('link');
+      for (var i = 0, l = nodeList.length; i < l; i++) {
+        var linkElement = nodeList[i];
+        if (linkElement.href.substr(-oldHRef.length) !== oldHRef) continue;
+        return linkElement;
+      }
+    }
+    /**
+     * replace link tag, otherwise issues with Google Chrome sourcemaps
+     */
+    function replaceOldLink(oldLinkElement) {
+      console.warn('WARN: DEPRECATED - CSS location needs updating from', oldHRef, 'to', newHRef);
+      var parent = oldLinkElement.parentNode;
+      parent.removeChild(oldLinkElement);
+      var newLinkElement = document.createElement('link');
+      newLinkElement.href = newHRef;
+      newLinkElement.rel = 'stylesheet';
+      parent.appendChild(newLinkElement);
+    }
+    /**
+     * wait for document to load otherwise link tag isn't available
+     */
+    if (!document.body) {
+      document.addEventListener('DOMContentLoaded', fixCSSLocation);
+    } else {
+      fixCSSLocation();
+    }
+  })();
+
+  function loadScript(url, callback) {
+    if (!url || typeof url !== 'string') return;
+    var script = document.createElement('script');
+    script.onload = callback;
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
+  };
+
+  // 0. Keep loadScript code to add into Adapt API later
+  window.__loadScript = loadScript;
+
+  // 2. Setup require for old-style module declarations (some code still uses these), configure paths then load JQuery
+  function setupRequireJS() {
+    requirejs.config({
+      map: {
+        '*': {
+          coreJS: 'core/js',
+          coreViews: 'core/js/views',
+          coreModels: 'core/js/models',
+          coreCollections: 'core/js/collections'
         }
-        return false;
-    })();
+      },
+      paths: {
+        polyfill: 'libraries/polyfill.min',
+        jquery: 'libraries/jquery.min',
+        underscore: 'libraries/underscore.min',
+        'underscore.results': 'libraries/underscore.results',
+        backbone: 'libraries/backbone.min',
+        'backbone.controller': 'libraries/backbone.controller',
+        'backbone.controller.results': 'libraries/backbone.controller.results',
+        'backbone.es6': 'libraries/backbone.es6',
+        handlebars: 'libraries/handlebars.min',
+        velocity: 'libraries/velocity.min',
+        imageReady: 'libraries/imageReady',
+        inview: 'libraries/inview',
+        scrollTo: 'libraries/scrollTo.min',
+        bowser: 'libraries/bowser',
+        'enum': 'libraries/enum',
+        jqueryMobile: 'libraries/jquery.mobile.custom'
+      }
+    });
+    loadJQuery();
+  }
 
-    //2. Setup require for old style module declarations
-    function setupRequireJS() {
-        requirejs.config({
-            map: {
-                '*': {
-                    coreJS: 'core/js',
-                    coreViews: 'core/js/views',
-                    coreModels: 'core/js/models',
-                    coreCollections: 'core/js/collections',
-                    coreHelpers: 'core/js/helpers',
-                    'coreJS/libraries/bowser': 'libraries/bowser',
-                    'core/js/libraries/bowser': 'libraries/bowser'
-                }
-            },
-            paths: {
-                underscore: 'libraries/underscore',
-                backbone: 'libraries/backbone',
-                handlebars: 'libraries/handlebars',
-                velocity: 'libraries/velocity',
-                imageReady: 'libraries/imageReady',
-                inview: 'libraries/inview',
-                a11y: 'libraries/jquery.a11y',
-                scrollTo: 'libraries/scrollTo',
-                bowser: 'libraries/bowser'
-            }
-        });
-        loadJQuery();
+  // 3. start loading JQuery, wait for it to be loaded
+  function loadJQuery() {
+    loadScript('libraries/jquery.min.js', checkJQueryStatus);
+  }
+
+  // 4. Wait until JQuery gets loaded completely then load foundation libraries
+  function checkJQueryStatus() {
+    if (window.jQuery === undefined) {
+      setTimeout(checkJQueryStatus, 100);
+    } else {
+      setupModernizr();
     }
+  }
 
-    //3. Load jquery
-    function loadJQuery() {
-        Modernizr.load([
-            {
-                test: IE == 8,
-                yep: 'libraries/jquery.js',
-                nope: 'libraries/jquery.v2.js',
-                complete: checkJQueryStatus
-            }
-        ]);
-    }
+  // 5. Backward compatibility for Modernizr
+  function setupModernizr() {
+    Modernizr.touch = Modernizr.touchevents;
+    var touchClass = Modernizr.touch ? 'touch' : 'no-touch';
+    $('html').addClass(touchClass);
+    loadFoundationLibraries();
+  }
 
-    //4. Wait until JQuery gets loaded completly
-    function checkJQueryStatus() {
-        if(window.jQuery === undefined) {
-            setTimeout(checkJQueryStatus, 100);
-        } else {
-            loadShim();
-        }
-    }
+  // 6. Load foundation libraries and templates then load Adapt itself
+  function loadFoundationLibraries() {
+    require([
+      'handlebars',
+      'polyfill',
+      'underscore',
+      'underscore.results',
+      'backbone',
+      'backbone.controller',
+      'backbone.controller.results',
+      'backbone.es6',
+      'velocity',
+      'imageReady',
+      'inview',
+      'jqueryMobile',
+      'libraries/jquery.resize',
+      'scrollTo',
+      'bowser',
+      'enum'
+    ], loadTemplates);
+  }
 
-    //5. Load IE 8 shim
-    function loadShim() {
-        Modernizr.load([
-            {
-                test: IE == 8,
-                yep: 'libraries/es5-shim.min.js',
-                nope: '',
-                complete: loadFoundationLibraries()
-            }
-        ]);
-    }
+  // 7. Load templates after making handlebars context global
+  function loadTemplates(Handlebars) {
+    window.Handlebars = Handlebars;
+    require([
+      'templates'
+    ], loadAdapt);
+  }
 
-    //6. Load foundation libraries and templates
-    function loadFoundationLibraries() {
-        require([
-            "underscore",
-            "backbone",
-            "handlebars",
-            "velocity",
-            "imageReady",
-            "inview",
-            "a11y",
-            "scrollTo",
-            "templates"
-        ], loadAdapt);
-    }
+  // 8. Allow cross-domain AJAX then load Adapt
+  function loadAdapt() {
+    $.ajaxPrefilter(function(options) {
+      options.crossDomain = true;
+    });
+    loadScript('adapt/js/adapt.min.js');
+  }
 
-    //7. Load adapt
-    function loadAdapt() {
-        switch (IE) {
-        case 8: case 9:
-            //ie8 and ie9 don't do crossdomain with jquery normally
-            break;
-        default:
-            //cross domain support for all other browers
-            $.ajaxPrefilter(function( options ) {
-                options.crossDomain = true;
-            });
-        }
-        Modernizr.load("adapt/js/adapt.min.js");
-    }
-
-    //1. Load foundation libraries, json2, consoles, requirejs
-    Modernizr.load([
-        {
-            test: window.JSON,
-            nope: 'libraries/json2.js'
-        },
-        {
-            test: window.console == undefined,
-            yep: 'libraries/consoles.js'
-        },
-        {
-            load: "libraries/require.js",
-            complete: setupRequireJS
-        }
-    ]);
+  // 1. Load requirejs then set it up
+  loadScript('libraries/require.min.js', setupRequireJS);
 
 })();
